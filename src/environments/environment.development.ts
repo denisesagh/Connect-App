067f4d90a72f5a49365903702d4beb1a13e37f95
denis <esagholian35@gmail.com>
2023-12-18 11:47:51 +0100

    Final

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'YOURPROJECTID',
+    appId: 'YOURAPPID',
+    storageBucket: 'YOURSTORAGE',
+    apiKey: 'YOURAPI',
+    authDomain: 'yourauthdomain',
+    messagingSenderId: '123456789',
+    databaseURL: 'yoururl'
+  },};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'YOURPROJECTID',
+    appId: 'YOURAPPID',
+    storageBucket: 'YOURSTORAGE',
+    apiKey: 'YOURAPI',
+    authDomain: 'yourauthdomain',
+    messagingSenderId: '123456789',
+    databaseURL: 'yoururl'
+  },};
